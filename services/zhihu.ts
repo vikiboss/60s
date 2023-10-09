8a407c14af94cd054fa529dac00a3cab247af050
Viki <hi@viki.moe>
2023-10-09 09:34:20 +0800

    fix: 修复知乎返回格式为 text 时，字段取值错误问题

--- services/zhihu.ts
+++ services/zhihu.ts
@@ -5,6 +5,6 @@ const api = 'https://www.zhihu.com/api/v4/search/top_search'
 export async function fetchZhihu(type = 'json') {
   const { top_search = {} } = await (await fetch(api)).json()
   const list: any[] = top_search?.words ?? []
-  const rawRes = list.map((e, i) => `${i + 1}. ${e?.word}`).join('\n')
+  const rawRes = list.map((e, i) => `${i + 1}. ${e?.query}`).join('\n')
   return type === 'json' ? responseWithBaseRes(list) : rawRes
 }
