a3c6d85b22ea94e2d59a72e26e673dfe65439bdf
Viki <hi@viki.moe>
2024-07-23 08:58:46 +0800

    chore: enrich debug middleware

--- src/middlewares/debug.ts
+++ src/middlewares/debug.ts
@@ -1,7 +1,12 @@
-import type { Context, Next } from 'oak'
+import type { Context, Next } from "oak";
 
 export default async function debug(ctx: Context, next: Next) {
-  // for debug
-  console.log(`[${new Date().toLocaleString('zh-CN')}] ${ctx.request.url.href}`)
-  await next()
+	// for debug
+	const ua = ctx.request.headers.get("user-agent") || "Unknown";
+	const ip = ctx.request.ip || "Unknown";
+	const date = new Date().toLocaleString("zh-CN");
+
+	console.log(`[${date}] [${ip}] [${ua}] ${ctx.request.url.href}`);
+
+	await next();
 }
