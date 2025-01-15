51c4454acfc2d92abaa368489fe2d2d9a7870b0d
Viki <hi@viki.moe>
2025-01-15 11:31:18 +0800

    chore: update message

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { customAlphabet } from 'nanoid'
 
-const defaultTips = '所有数据均来自官方，确保稳定与实时，用户群: 595941841，开源地址: https://github.com/vikiboss/60s'
+const defaultTips = 'v1 版本即将在 2025/6/30 号后下线，请及时迁移到 v2。所有数据均来自官方，确保稳定与实时，用户群: 595941841，开源地址: https://github.com/vikiboss/60s'
 
 export const randomId = (size: number) => customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', size)
 
