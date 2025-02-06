32050aa010884f0ef07dc18c04b45f5f44e7bb18
Viki <hi@viki.moe>
2025-02-06 14:53:51 +0800

    fix: fix ck

--- test/zhihu.ts
+++ test/zhihu.ts
@@ -11,8 +11,7 @@ fetch('https://www.zhihu.com/people/98-18-69-57/posts', {
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    cookie:
-      'SESSIONID=N9867psGL8lVfpp9u8VldvtEXQGyviNfLNL64gQBG76; SESSIONID=xQEGd39LNxgH1culz56fDzVzbEbB2xXe59EJdXrx0q7; _xsrf=37SBHIVaV1of6XDICO0bSCB3mWWa4M1m; _zap=217bb0a1-9ff2-43b6-a827-4d928a8c4405; d_c0=ACBSOkp7zRiPTkph8h5vOMaZoWDoxOBpOCc=|1718886497; __snaker__id=w1Zujd3sWUUxcCWl; q_c1=c6369ee5593b4e97ac9cae132f648530|1723030753000|1723030753000; z_c0=2|1:0|10:1738723912|4:z_c0|80:MS4xWW9zMVZRQUFBQUFtQUFBQVlBSlZUVWNna0dpaEdWeVk0VTNqNnZBd18yS01NU0ZkSzVVYVpBPT0=|a47fa2ce39370655be92f521c3dd20a8527df217fdc710c08967b79ecb14c46d; tst=h; SESSIONID=BSXen2sVYtRINnSuCMtdCPPWGho6QM8mpSRMU5DdaO6; __zse_ck=004_4JOckzGYXLTAFG1aCCL2z1AOetxz=6EA5gF/E83l5kXnm1O=LZahm1MoBl8E1oLceBWyDLPZp8tHBDtpHersByitNyjwCEdnU5pMPT0/dYJLSrLfeGwqOM7zCKDGsQC8-wdK7pXvT+BldQBjbS+KR9fF0lTNAIl4BaQpqYUW1//J+jcKVD4GXJ00CAxVnG625opoL44lAymXrnhXyfhK8HzQFuZ3Ui3yPxAj2QYlrLEIPYxgpBjlujh5+eEmcY1Vf; BEC=6ff32b60f55255af78892ba1e551063a',
+    cookie: '',
     Referer: 'https://www.zhihu.com/people/98-18-69-57',
     'Referrer-Policy': 'no-referrer-when-downgrade',
     'User-Agent':
