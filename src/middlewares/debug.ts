cf8f36d04b2a38b06c0918a7feb7df97e848c281
Viki <hi@viki.moe>
2025-02-21 17:05:49 +0800

    feat: add referer

--- src/middlewares/debug.ts
+++ src/middlewares/debug.ts
@@ -3,10 +3,11 @@ import type { Context, Next } from "@oak/oak";
 export default async function debug(ctx: Context, next: Next) {
 	// for debug
 	const ua = ctx.request.headers.get("user-agent") || "Unknown";
+  const referer = ctx.request.headers.get("referer") || "Unknown";
 	const ip = ctx.request.ip || "Unknown";
 	const date = new Date().toLocaleString("zh-CN");
 
-	console.log(`[${date}] [${ip}] [${ua}] ${ctx.request.url.href}`);
+	console.log(`[${date}] [${referer}] [${ip}] [${ua}] ${ctx.request.url.href}`);
 
 	await next();
 }
