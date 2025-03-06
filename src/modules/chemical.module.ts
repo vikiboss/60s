77d28636ad773d0834dacc2f83f4726e3dbefc0a
Viki <hi@viki.moe>
2025-03-06 19:03:29 +0800

    fix: fix chemical

--- src/modules/chemical.module.ts
+++ src/modules/chemical.module.ts
@@ -4,7 +4,7 @@ import type { RouterMiddleware } from '@oak/oak'
 
 class ServiceChemical {
   handle(): RouterMiddleware<'/chemical'> {
-    return async (ctx) => {
+    return async ctx => {
       const id = ctx.request.url.searchParams.get('id') || Common.randomInt(1, 60_000_000)
 
       // 太复杂了，不适合
@@ -12,15 +12,18 @@ class ServiceChemical {
 
       const res = await fetch(`https://www.chemspider.com/Chemical-Structure.${id}.html`)
       const html = await res.text()
-      const data = JSON.parse(html.split('id="__NUXT_DATA__" data-ssr="true">')[1]?.split('</script>')[0] || '[]')
+      // console.log(html)
+      // const data = JSON.parse(html.split('id="__NUXT_DATA__"【】>')[1]?.split('</script>')[0] || '[]'
+      const data = JSON.parse(/id="__NUXT_DATA__"[^>]*>([^<]*)</.exec(html)?.[1] || '[]')
+      // console.log('=====\n\n', data[7], data[14], data[11], data[15])
 
       ctx.response.body = Common.buildJson({
         id: +id,
-        name: data[7] || '',
-        mass: data[14] ? toFixedNumber(data[14], 3) : '',
-        formula: data[11] || '',
+        name: data[9] || '',
+        mass: data[16] ? toFixedNumber(data[16], 3) : '',
+        formula: data[13] || '',
         image: `https://legacy.chemspider.com/ImagesHandler.ashx?id=${id}`,
-        monoisotopicMass: data[15] ? toFixedNumber(data[15], 3) : '',
+        monoisotopicMass: data[17] ? toFixedNumber(data[17], 3) : '',
       })
     }
   }
