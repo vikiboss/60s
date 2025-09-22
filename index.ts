8680de43805d24225dadd1d1aded7574eb626eb2
zhangchunhui <boycot2017@163.com>
2025-09-22 17:51:14 +0800

    ✨ feat(index): 新增应用启动逻辑
    - 引入 `app` 和 `config` 模块
    - 实现 `startApp` 函数，启动应用并监听配置端口

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-import './node.ts'
+import { app } from './src/app.ts'
+import { config } from './src/config.ts'
+
+const startApp = async () => {
+  await app.listen({
+    hostname: config.host,
+    port: config.port,
+  })
+  console.log(`service is running at http://localhost:${config.port}`)
+}
+startApp()
