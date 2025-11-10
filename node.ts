2cba4b830339b77c309b629f5e5d5d6d199eeaf1
Viki <hi@viki.moe>
2025-11-10 14:12:13 +0800

    chore: refine dev log

--- node.ts
+++ node.ts
@@ -1,7 +1,7 @@
 import { app } from './src/app.ts'
 import { config } from './src/config.ts'
 
-console.log(`service is running at http://${config.host}:${config.port}`)
+console.log(`service is running at http://localhost:${config.port}`)
 
 await app.listen({
   hostname: config.host,
