e3f370aed23ba323279a4a458fc193f8b869e0d0
Viki <hi@viki.moe>
2026-01-05 11:27:30 +0800

    chore: 优化 debug

--- src/middlewares/debug.ts
+++ src/middlewares/debug.ts
@@ -4,12 +4,16 @@ import type { Middleware } from '@oak/oak'
 
 export function debug(): Middleware {
   return async (ctx, next) => {
-    const ua = ctx.request.headers?.get('user-agent') || '-'
+    const ua = ctx.request.headers?.get('user-agent') || ''
+    const referrer = ctx.request.headers?.get('referer') || ''
     const ip = serviceIP.getClientIP(ctx.request.headers) || ctx.request.ip || '-'
+    const url = ctx.request.url.href || ''
+    const method = ctx.request.method || ''
     const date = new Date().toLocaleString('zh-CN')
 
-    console.log(`[${date}] [${ip}] UA: ${ua}`)
-    console.log(`[${date}] [${ip}] URL: ${ctx.request.url.href}`)
+    console.log(`[${date}] [${ip}] ${method.toUpperCase()} ${url} (${ua || '未知 UA'})`)
+
+    if (referrer) console.log(`[${date}] [${ip}] Referrer: ${referrer}`)
 
     await next()
   }
