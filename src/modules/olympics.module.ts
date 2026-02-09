dec4ee2648b9e5cd876f45271da0d38de443248a
Viki <hi@viki.moe>
2026-02-09 15:17:22 +0800

    fix: 修复

--- src/modules/olympics.module.ts
+++ src/modules/olympics.module.ts
@@ -104,6 +104,7 @@ ${rows.join('\n')}`
         'User-Agent':
           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36',
       },
+      redirect: 'manual',
     })
 
     if (!response.ok) {
