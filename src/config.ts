a04a870230de37770577f63878127115fa659284
Viki <hi@viki.moe>
2026-04-22 11:54:54 +0800

    docs: 更新文档和通知

--- src/config.ts
+++ src/config.ts
@@ -9,4 +9,4 @@ export const config = {
   encodingParamName: process.env.ENCODING_PARAM_NAME || 'encoding',
 }
 
-export const COMMON_MSG = `获取成功，开源地址 ${config.github}，反馈群 ${config.group}`
+export const COMMON_MSG = `获取成功。当前部署平台 Deno Deploy Classic 预计将于 7 月 20 日 终止服务，届时公共 API 服务（60s.viki.moe）可能出现中断。建议尽快迁移至私有部署，以确保服务持续可用。开源地址 ${config.github}，反馈群 ${config.group}。`
--- src/router.ts
+++ src/router.ts
@@ -109,6 +109,7 @@ appRouter.get('/rednote', serviceRednote.handle())
 appRouter.get('/dongchedi', serviceDongchedi.handle())
 appRouter.get('/moyu', serviceMoyu.handle())
 appRouter.get('/quark', serviceQuark.handle())
+appRouter.get('/whois', serviceWhois.handle())
 
 appRouter.get('/health', serviceHealth.handle())
 appRouter.get('/password', servicePassword.handle())
@@ -154,7 +155,6 @@ appRouter.all('/hash', serviceHash.handle())
 
 appRouter.all('/fanyi', serviceFanyi.handle())
 appRouter.all('/fanyi/langs', serviceFanyi.handleLangs())
-appRouter.get('/whois', serviceWhois.handle())
 
 // === 以下为测试接口，beta 前缀，接口可能不稳定 ===
 appRouter.get('/beta/kuan', serviceKuan.handle())
